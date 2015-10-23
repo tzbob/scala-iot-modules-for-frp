69248d5da2d1b23431083f2bc001597528811ebf
Ben Calus <ben.calus@student.kuleuven.be>
2015-10-23 03:52:19 +0200

    FRP API : Event (non-LMS version)

--- src/main/scala/FRPDSL.scala
+++ src/main/scala/FRPDSL.scala
@@ -1,35 +1,58 @@
-import scala.lms.common.Base
+import scala.lms.common.{LiftAll, Base}
 
-trait FRPDSL extends Event
+trait FRPDSL extends EventOps
 
+trait EventOps {
+  trait Event[A] extends Base with LiftAll {
+    def constant[B] (c: B): Event[B]
+    def map[B] (f: A => B): Event[B]
+    def filter (f: A => Boolean): Event[A]
+  }
 
-trait Event[A] extends Base {
-  def constant[B] (c: Rep[B]): Event[B]
-  def map[B] (f: Rep[A => B]): Event[B]
-  def filter (f: Rep[A => Boolean]): Event[A]
+  def TimerEvent(i: Int): Event[Int]
+
+  def print[A](e: Event[A]): String
 }
 
-trait EventImpl[A] extends Event[A] {
-  abstract class EventNode[A,B] extends EventImpl[A] {
-    val updateFunc: Rep[A => B]
-    val parentEvents: Event = this
-    val childEvents: Event = _ // immutable so build from bottom to top!
+trait EventOpsImpl extends EventOps {
+
+  override def print[A](e: Event[A]) = ???
+
+  override def TimerEvent(i: Int) = new EventNode[Int, Int] with LMSImpl {
+    override val updateFunc: (Int) => Int = _ //TODO: do something with the input
+    override val parentEvents: List[EventNode[Int, Int]] = Nil
+  }
+
+  abstract class EventNode[A,B] extends EventImpl[B] {
+    val updateFunc: A => B //TODO: should be Rep[A=>B], replaced because of pulse function
+    val parentEvents: List[EventNode[B,A]]
+    val childEvents: List[EventNode[B,A]] = _ // immutable so build from bottom to top! NEEDED?
     // val rank // Used for topological order --> glitch prevention
+    def pulse(x: A): Unit = {
+      val update = updateFunc(x)
+      for(p <- parentEvents) p.pulse(update)
+    }
   }
 
-  case class ConstantEvent[A,B](c : Rep[B]) extends EventNode[A,B] with LMSImpl {
-    override val updateFunc: (A) => B = (_ => c)
-  }
-  case class MapEvent[A,B](f: Rep[A=>B]) extends EventNode[A,B] with LMSImpl {
-    override val updateFunc: (A) => B = f
-  }
-  case class FilterEvent[A,B](f: Rep[A => Boolean]) extends EventNode[A,B] with LMSImpl {
-    override val updateFunc: (A) => A = ???
-  }
+  trait EventImpl[A] extends Event[A] {
 
-  override def constant[B](c: Rep[B]): Event[A] = new ConstantEvent[A,B](c)
-  override def map[B](f: Rep[(A) => B]): Event[A] = new MapEvent[A,B](f)
-  override def filter(f: Rep[(A) => Boolean]): Event[A] = new FilterEvent[A,A](f)
+    case class ConstantEvent[A,B](parent: Event[A], c : B) extends EventNode[A,B] with LMSImpl {
+      override val parentEvents: List[Event[A]] = List(parent)
+      override val updateFunc: A=>B = _ => c //TODO: must be lifted to Rep
+    }
+    case class MapEvent[A,B](parent: Event[A], f: A=>B) extends EventNode[A,B] with LMSImpl {
+      override val parentEvents: List[Event[A]] = List(parent)
+      override val updateFunc: A=>B = f
+    }
+    case class FilterEvent[A](parent: Event[A], f: A => Boolean) extends EventNode[A,A] with LMSImpl {
+      override val parentEvents: List[Event[A]] = List(parent)
+      override val updateFunc: A=>Option[A] = (input:A) => if(f(input)) Some(input) else None //TODO: make updateFunc give Option[A] back
+    }
+
+    override def constant[B](c: B): Event[B] = new ConstantEvent[A,B](this, c)
+    override def map[B](f: (A) => B): Event[B] = new MapEvent[A,B](this, f)
+    override def filter(f: (A) => Boolean): Event[A] = new FilterEvent[A](this, f)
+  }
 }
 
 trait LMSImpl extends Base {
@@ -52,5 +75,5 @@ trait BehaviorOps extends Base {
 /*
  * Implementation of the FRP DSL
  */
-trait FRPDSLImpl extends FRPDSL with EventImpl {
+trait FRPDSLImpl extends FRPDSL with EventOpsImpl {
 }
--- src/main/scala/MyFirstApp.scala
+++ src/main/scala/MyFirstApp.scala
@@ -2,8 +2,8 @@
 trait myFirstApp extends FRPDSLApplication {
   override def main() {
     println("My first application executes")
-    val x : Event[Double] = TimerEvent(5) // every 5 sec
-    //val y = x.map(lift(_ => 2*_))
+    val x : Event[Int] = TimerEvent(5) // every 5 sec
+    val y = x.map[String]( ((i:Int) => i.toString) )
   }
 }
 
