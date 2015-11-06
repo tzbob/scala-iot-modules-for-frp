aee09cc792004bcd1bf30a601e5459812bcb52dd
Ben Calus <ben.calus@student.kuleuven.be>
2015-11-06 21:04:55 +0100

    FRP API : extend Behavior API - add comments

--- src/main/scala/be/kuleuven/FRP_EMBEDDED/BehaviorOps.scala
+++ src/main/scala/be/kuleuven/FRP_EMBEDDED/BehaviorOps.scala
@@ -6,14 +6,29 @@ trait BehaviorOps {
   event: EventOps =>
 
   trait Behavior[A] extends Base {
-    def valueNow(): A
-    def delay(t: Rep[Int]): Behavior[A]
+    def valueNow (): A
 
-    def snapshot[B](e: Event[B]): Event[A]
-    def changes(): Event[A]
+    def delay (t: Rep[Int]): Behavior[A]
+
+    // map2: Combine 2 behaviors (merge-like). Whenever a behaviors value changes, the output behavior applies
+    // the function f to both child behaviors, changing its value to the result of that function
+    def map2[B,C] (b: Behavior[B], f: (A, B)=>C ): Behavior[C]
+    //def map3
+    //def map4
+    //...
+
+    // General snapshot: Output event fires an event containing result of function of current value of behavior,
+    // whenever the input event e (carrying the function to be applied to value of behavior) fires an event
+    def genSnapshot[B] (e: Event[A=>B]): Event[B]
+    // Snapshot: Output event fires an event containing the current value of behavior,
+    // each time input e fires an event
+    def snapshot[B] (e: Event[B]): Event[A]
+    // changes: create an event stream that fires an event each time the value of the behavior changes,
+    // carrying the new value
+    def changes (): Event[A]
   }
 
-  def constantB[A](value: A): Behavior[A]
+  def constantB[A] (value: A): Behavior[A]
 }
 
 trait BehaviorOpsImpl extends BehaviorOps {
@@ -33,5 +48,7 @@ trait BehaviorOpsImpl extends BehaviorOps {
 
     override def snapshot[B](e: Event[B]): Event[A] = ???
     override def changes(): Event[A] = ???
+    override def map2[B, C](b: Behavior[B], f: (A, B) => C): Behavior[C] = ???
+    override def genSnapshot[B](e: Event[(A) => B]): Event[B] = ???
   }
 }
