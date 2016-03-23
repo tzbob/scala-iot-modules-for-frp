33c76c18dedf6e57a667a7f41635130a8a37d65e
Ben Calus <ben.calus@student.kuleuven.be>
2016-03-23 21:21:40 +0100

    SMC : update SMCInput app

--- generated/SMCInputApp.c
+++ generated/SMCInputApp.c
@@ -5,17 +5,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-int32_t x0 = 5;
-SM_INPUT(module1,x13,x1,x2) {
-int32_t x5 = 10;
-int32_t x3 = x1;
-int32_t x6 = x3 + 1;
-printf("%d\n",x6);
-int32_t x8 = x0;
-printf("%d\n",x8);
-int32_t x10 = x5;
-printf("%d\n",x10);
-};
+int x0 = 5;
+SM_DATA(module2) int x1;
+SM_INPUT(module2,top1,x2,x3) {
+int x6 = 10;
+int x4 = x2;
+int x7 = x4 + 1;
+printf("%d\n",x7);
+int x9 = x0;
+printf("%d\n",x9);
+int x11 = x6;
+printf("%d\n",x11);
+int x13 = x1;
+printf("%d\n",x13);
+}
 /*****************************************
   End of C Generated Code                  
 *******************************************/
--- src/main/scala/be/kuleuven/Applications/SMCApps.scala
+++ src/main/scala/be/kuleuven/Applications/SMCApps.scala
@@ -14,33 +14,6 @@ object SMCAppsRunner {
         val program: () => Rep[Unit] =
           () => {
             val xx = var_new[Int](5)
-
-            val infun = inputfun ("module1", "top1") { (x:Rep[Int], y:Rep[Int]) =>
-              val yy = var_new[Int](10)
-              println(x+1)
-              println(xx)
-              println(yy)
-              unitToRepUnit( () )
-            }
-            doApplyDecl(infun)
-
-            unitToRepUnit( () )
-          }
-
-        System.err.println("\n")
-        emitProgram(program)
-        System.err.println("%%%%%%%%%%%%%%%%%%%%%%%%%%")
-        System.err.println("\n\n")
-      }
-    }
-
-    withOutFile("SMCTupledInputApp.c") {
-      new SMCFRPDSLApplicationrunner {
-        System.err.println("%%%%%%%%%%%%%%%%%%%%%%%%%%")
-
-        val program: () => Rep[Unit] =
-          () => {
-            val xx = var_new[Int](5)
             val modvar = vardeclmod_new[Int]("module2")
 
             val infun = inputfun ("module2", "top1") { (x:Rep[Int],y:Rep[Int]) =>
