2033a0d8a4d979544118aa2a32399e6816b489fe
Ben Calus <ben.calus@student.kuleuven.be>
2016-04-13 00:36:53 +0200

    Events : function values not supported

---
+++ src/main/scala/be/kuleuven/Applications/LMSFunctionApp.scala
@@ -0,0 +1,39 @@
+package be.kuleuven.Applications
+
+import be.kuleuven.FRP_EMBEDDED.{CFRPDSLApplicationRunner, OutputGenerator, FRPDSLApplication}
+
+trait LMSFunction1App extends FRPDSLApplication {
+
+  createModule ("module1") { () =>
+    val t1: Event[Int] = TimerEvent(5) // every 5 sec
+    val n1 = t1.map[Int=>Int]( (i) => (x: Rep[Int]) => x+1 )
+    // FUNCTION values not suppported because of static functions in C
+    val n2: Behavior[Int] = n1.foldp[Int]( (x, f) => f(x), 0)
+  }
+}
+
+import OutputGenerator.withOutFile
+
+object LMSFunctionAppRunner {
+
+  def main(args: Array[String]): Unit = {
+
+    withOutFile("LMSFunction1App.c") {
+      // running this generates an exception because of unsupported Function1 type in remapping to C
+      new LMSFunction1App with CFRPDSLApplicationRunner {
+        System.err.println("%%%%%%%%%%%%%%%%%%%%%%%%%%")
+        System.err.println("Function1App:")
+        System.err.println("Creating flow graph...")
+        //createMainFun
+        System.err.println("\n")
+        //buildFRPGraph()
+        System.err.println("\n")
+        val program = buildProgram()
+        System.err.println("\n")
+        //emitProgram(program)
+        System.err.println("%%%%%%%%%%%%%%%%%%%%%%%%%%")
+        System.err.println("\n\n")
+      }
+    }
+  }
+}
