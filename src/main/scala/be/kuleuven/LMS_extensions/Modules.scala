de6afd07da120551d5f4c28c9b9afa5638ccca5a
Ben Calus <ben.calus@student.kuleuven.be>
2016-05-11 01:52:26 +0200

    add support for all new kinds of generation steps

--- src/main/scala/be/kuleuven/LMS_extensions/Modules.scala
+++ src/main/scala/be/kuleuven/LMS_extensions/Modules.scala
@@ -5,9 +5,11 @@ import scala.lms.common.{CGenEffect, EffectExp, Base}
 trait Modules extends Base {
 
   def declare_module(modName: String): Rep[Unit]
-  def globalInitFun(): Rep[Unit]
-  def deployFun(modNames: List[String], outInList: List[(String,String,String,String)]): Rep[Unit]
-  def mainFun(): Rep[Unit]
+  def systemInits(): Rep[Unit]
+  def moduleDeploy(modNames: List[String]): Rep[Unit]
+  def connectionDeploy(modOut: String, outfun: Rep[_], modIn: String, infun: Rep[_]): Rep[Unit]
+  def registerButton(id: Int, cb: Rep[(Int)=>Unit]): Rep[Unit]
+  def eventLoop(): Rep[Unit]
   def headers(): Rep[Unit]
 
 }
@@ -16,8 +18,10 @@ trait ModulesExp extends Modules with ExpressionsExt with EffectExp {
 
   case class ModuleDecl(name: String) extends Def[Unit]
   case class GlobalInit() extends Def[Unit]
-  case class Deploy(modNames: List[String], outInList: List[(String,String,String,String)]) extends Def[Unit]
-  case class Main() extends Def[Unit]
+  case class ModuleDeploy(modNames: List[String]) extends Def[Unit]
+  case class ConnectionDeploy(modOut: String, outfun: Exp[_], modIn: String, infun: Exp[_]) extends Def[Unit]
+  case class RegisterButton(id:Int, cb: Rep[(Int)=>Unit]) extends Def[Unit]
+  case class EventLoop() extends Def[Unit]
   case class Headers() extends Def[Unit]
 
   override def declare_module(n: String): Rep[Unit] = {
@@ -25,18 +29,28 @@ trait ModulesExp extends Modules with ExpressionsExt with EffectExp {
     Const()
   }
 
-  override def globalInitFun(): Rep[Unit] = {
+  override def systemInits(): Rep[Unit] = {
     reflectEffect(new GlobalInit())
     Const()
   }
 
-  override def deployFun(modNames: List[String], outInList: List[(String,String,String,String)]): Rep[Unit] = {
-    reflectEffect(new Deploy(modNames,outInList))
+  override def moduleDeploy(modNames: List[String]): Rep[Unit] = {
+    reflectEffect(new ModuleDeploy(modNames))
     Const()
   }
 
-  override def mainFun(): Rep[Unit] = {
-    reflectEffect(new Main())
+  override def connectionDeploy(modOut: String, outfun: Exp[_], modIn: String, infun: Exp[_]): Exp[Unit] = {
+    reflectEffect(new ConnectionDeploy(modOut, outfun, modIn, infun))
+    Const()
+  }
+
+  override def registerButton(id: Int, cb: Rep[(Int)=>Unit]): Rep[Unit] = {
+    reflectEffect(new RegisterButton(id,cb))
+    Const()
+  }
+
+  override def eventLoop(): Rep[Unit] = {
+    reflectEffect(new EventLoop())
     Const()
   }
 
@@ -54,17 +68,10 @@ trait CGenModules extends CGenEffect with SMCLikeCodeGen {
   override def emitNode(sym: Sym[Any], rhs: Def[Any]) = rhs match {
     case ModuleDecl(n) => // no modules in standard C
     case GlobalInit() => // nothing
-    case Deploy(l, oil) => // nothing
-    case Main() => /*stream.println(
-      "\nint main()"+
-        "\n"+
-        "{\n"+
-        "  puts(\"main started\");\n"+
-        "  \n\n" +
-        "  //ADD CODE HERE\n"+
-        "  \n\n" +
-        "}"
-    )*/
+    case ModuleDeploy(l) => // nothing
+    case ConnectionDeploy(a,b,c,d) => // nothing
+    case RegisterButton(_,_) => //nothing
+    case EventLoop() => // nothing
     case Headers() =>
       stream.println(
         "#include <stdio.h>\n" +
@@ -84,40 +91,32 @@ trait SMCGenModules extends CGenEffect with SMCLikeCodeGen {
   override def emitNode(sym: Sym[Any], rhs: Def[Any]) = rhs match {
     case ModuleDecl(n) => stream.println("DECLARE_SM(" + n + ", 0x1234);")
     case GlobalInit() => stream.println(
-      "\nstatic void init()"+
-      "\n"+
-      "{\n"+
-      "  WDTCTL = WDTHOLD | WDTPW;\n"+
-      "  uart_init();\n"+
-      "}"
+      "  //INIT FUNCTION\n" +
+        "  WDTCTL = WDTHOLD | WDTPW;\n" +
+        "  uart_init();\n" +
+        "  pmodcls_init();\n" +
+        "  pmodcls_set_wrap_mode(PmodClsWrapAt16);\n" +
+        "  buttons_init();\n" +
+        "  asm(\"eint\");"
     )
-    case Deploy(l, oil) => {
+    case ModuleDeploy(l) => {
       val strB = new StringBuilder
-      strB.append("\nstatic void deploy()")
-      strB.append("\n")
-      strB.append("{\n")
+      strB.append("  //DEPLOY FUNCTION\n")
       for( n <- l ) {
         strB.append("  sancus_enable(&" + n + ");\n")
-        strB.append("  sm_register_existing(&" + n +");\n")
+        strB.append("  sm_register_existing(&" + n +");")
       }
-      for( (modOut, nameOut, modIn, nameIn) <- oil) {
-        strB.append("  REACTIVE_CONNECT(" + modOut + ", " + nameOut + ", " + modIn + ", " + nameIn +");\n")
-      }
-      strB.append("}\n")
       stream.println(strB.toString())
     }
-    case Main() => stream.println(
-      "\nint main()"+
-        "\n"+
-        "{\n"+
-        "  init();\n"+
-        "  puts(\"main started\");\n"+
-        "  deploy();\n"+
-        "  \n\n" +
-        "  //ADD CODE HERE\n"+
-        "  \n\n" +
-        "}"
-    )
+    case ConnectionDeploy(modOut, outfun, modIn, infun) =>
+      stream.println("  REACTIVE_CONNECT(" + modOut + ", " + quote(outfun) + ", " + modIn + ", " + quote(infun) + ");")
+    case EventLoop() =>
+      stream.println(
+        "while(1)\n" +
+        "  buttons_handle_events();"
+      )
+    case RegisterButton(id, cb) =>
+      stream.println("buttons_register_callback(Button" + id + "," +  quote(cb) + ");")
     case Headers() =>
       stream.println(
         "#include <sancus/sm_support.h>\n\n" +
